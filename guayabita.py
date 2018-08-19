dd79fd7f8353bf61cde7617c0bbd5acc6e5b921c
Andres Bayuelo <jabayuelor@correo.udistrital.edu.co>
2018-08-19 18:15:18 -0500

    Version (1.0) del algoritmo

--- guayabita.py
+++ guayabita.py
@@ -1,38 +1,43 @@
 import random
 
-def apuestaAleatoria(dineroMesa, montoJugador):
+def f1(dineroMesa, minimaApuesta, dineroJugador1, dineroJugador2, jugador):
+    print ("dinero mesa -> " + str(dineroMesa) + ", dinero jugador 1 -> " + str(dineroJugador1) + ", dinero jugador 2 -> " + str(dineroJugador2) + ", turno jugador -> " + str(jugador))
+    if dineroJugador1 != 0 and dineroJugador2 != 0:
+        if jugador==1:
+            return f4(f2((random.randrange(6)+1), 0, dineroMesa, dineroJugador1, minimaApuesta, 0), dineroMesa, minimaApuesta, dineroJugador1, dineroJugador2, jugador)
+        else:
+            return f4(f2((random.randrange(6)+1), 0, dineroMesa, dineroJugador2, minimaApuesta, 0), dineroMesa, minimaApuesta, dineroJugador1, dineroJugador2, jugador)
+    else:
+        return "fin del juego"
+
+def f2(valorDado, valorDadoAnt, dineroMesa, dineroJugador, minimaApuesta, valorApuesta):
+    print ("valor dado -> " + str(valorDado))
+    if valorApuesta > 0:
+        print ("valor apuesta -> " + str(valorApuesta))
+        if valorDado > valorDadoAnt:
+            return valorApuesta
+        else:
+            return 0-valorApuesta
+    elif valorDado == 1 or valorDado == 6:
+        if dineroJugador >= minimaApuesta:
+            return  0 - minimaApuesta
+        else:
+            return 0 - dineroJugador
+    elif (valorDado == 2 or valorDado == 3 or valorDado == 4 or valorDado == 5) and dineroMesa >= minimaApuesta:
+        return f2((random.randrange(6)+1), valorDado, dineroMesa, dineroJugador, minimaApuesta, f3(dineroMesa, dineroJugador))
+    else:
+        return 0
+
+def f3(dineroMesa, montoJugador):
     if montoJugador<=dineroMesa:
         return (random.randrange(montoJugador/100)+1)*100
     else:
         return (random.randrange(dineroMesa/100)+1)*100
 
-def guayabita(valorDadoAnt, dineroMesa, montoJugador1, montoJugador2, jugador, valorApostado): 
-    if montoJugador1==0 or montoJugador2==0: 
-        print("Jugador1->" + montoJugador1 + ", Jugador2->" + montoJugador2)
-    elif dineroMesa == 0:
-        return guayabita(0, dineroMesa+200, montoJugador1-100, montoJugador2-100, jugador, 0) 
-    elif valorApostado > 0:
-        if(random.randrange(6)+1) < valorDadoAnt: 
-            if jugador==1: 
-                return guayabita(0, dineroMesa+valorApostado, montoJugador1-valorApostado, montoJugador2, 2, 0) 
-            else: 
-                return guayabita(0, dineroMesa+valorApostado, montoJugador1, montoJugador2-valorApostado, 1, 0)
-        else:
-            if jugador==1: 
-                return guayabita(0, dineroMesa-valorApostado, montoJugador1+valorApostado, montoJugador2, 2, 0) 
-            else: 
-                return guayabita(0, dineroMesa-valorApostado, montoJugador1, montoJugador2+valorApostado, 1, 0)
-    elif valorDadoAnt == 0: 
-        return guayabita(random.randrange(6)+1, dineroMesa, montoJugador1, montoJugador2, jugador, 0)
-    elif valorDadoAnt == 1 or valorDadoAnt == 6: 
-        if jugador==1: 
-            return guayabita(0, dineroMesa+100, montoJugador1-100, montoJugador2, 2, 0) 
-        else: 
-            return guayabita(0, dineroMesa+100, montoJugador1, montoJugador2-100, 1, 0)
-    elif valorDadoAnt == 2 or valorDadoAnt == 3 or valorDadoAnt == 4 or valorDadoAnt == 5:
-        if jugador==1: 
-            return guayabita(valorDadoAnt, dineroMesa, montoJugador1, montoJugador1, jugador, apuestaAleatoria(dineroMesa, montoJugador1))
-        else: 
-            return guayabita(valorDadoAnt, dineroMesa, montoJugador1, montoJugador2, jugador, apuestaAleatoria(dineroMesa, montoJugador2))
+def f4(resultadoJuego, dineroMesa, minimaApuesta, dineroJugador1, dineroJugador2, jugador):
+    if jugador==1:
+        return f1(dineroMesa-resultadoJuego, minimaApuesta, dineroJugador1+resultadoJuego, dineroJugador2, 2)
+    else:
+        return f1(dineroMesa-resultadoJuego, minimaApuesta, dineroJugador1, dineroJugador2+resultadoJuego, 1)
 
-guayabita(0, 0, 1000, 1000, 1, 0)
+print (f1(400, 100, 1000, 1000, 1))
